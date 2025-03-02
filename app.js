cf29efff572f1dac89c819a995427c2d06c1bfe2
Brittany Halterman <halterma@gmail.com>
2025-03-02 13:10:24 -0500

    Handle status code errors

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const https = require('https');
+const http = require('http');
+
 // Print the data
 function printMessage(username, badgeCount, points) {
   const message = `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript`;
   console.log(message);
+}
+
+function printError(error) {
+  console.error(error.message);
 }
 
 function getProfile(username) {
@@ -11,32 +17,38 @@ function getProfile(username) {
     const request = https.get(
       `https://teamtreehouse.com/profiles/${username}.json`,
       (response) => {
-        let body = '';
-        // Read the data
-        response.on('data', (data) => {
-          body += data.toString();
-        });
-        response.on('end', () => {
-          try {
-            // Parse the data
-            let profile = JSON.parse(body);
+        if (response.statusCode == 200) {
+          let body = '';
+          // Read the data
+          response.on('data', (data) => {
+            body += data.toString();
+          });
+          response.on('end', () => {
+            try {
+              // Parse the data
+              let profile = JSON.parse(body);
 
-            printMessage(
-              username,
-              profile.badges.length,
-              profile.points.JavaScript
-            );
-          } catch (error) {
-            console.error(error.message);
-          }
-        });
+              printMessage(
+                username,
+                profile.badges.length,
+                profile.points.JavaScript
+              );
+            } catch (error) {
+              printError(error);
+            }
+          });
+        } else {
+          const message = `There was an error getting the profile for ${username} (${
+            http.STATUS_CODES[response.statusCode]
+          })`;
+          const statusCodeError = new Error(message);
+          printError(statusCodeError);
+        }
       }
     );
-    request.on('error', (error) =>
-      console.error('The requested address was not found')
-    );
+    request.on('error', (error) => printError(error));
   } catch (error) {
-    console.error(error.message);
+    printError(error);
   }
 }
 const users = process.argv.slice(2);
